e9dae9150e1a20a28b71ffbc141b0bdabfe8dc28
Alkenso <alkensox@gmail.com>
2018-03-08 17:55:48 +0200

    Added licence texts into the samples

--- samples/ClassBackends.cpp
+++ samples/ClassBackends.cpp
@@ -1,13 +1,23 @@
-//
-//  main.cpp
-//  EventLog
-//
-//  Created by Vladimir Vashurkin on 2/7/18.
-//  Copyright © 2018 Vladimir Vashurkin. All rights reserved.
-//
+/*
+ * Copyright (c) 2018 Alkenso (Vashurkin Vladimir), Alexander Smoktal
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #include <iostream>
 #include <sstream>
+
+#include <syslog.h>
 
 #include <Everlog/Everlog.h>
 
@@ -16,7 +26,7 @@ class SQLiteDb
 public:
     void execQuery(const std::string& query)
     {
-        std::cout << "Execute query: " << query << std::endl;
+        std::cout << "Execute query: " << query << '\n';
     }
 };
 
@@ -79,6 +89,8 @@ int main()
     logger.addBackend(SQLiteDb());
     
     LOG_ERROR(LogEvent("Something happened..."));
+    
+    syslog(<#int#>, <#const char *, ...#>)
     
     return 0;
 }
